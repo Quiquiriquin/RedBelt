3ae5075c86bf23fb1ad91c72ed18f0922f2627f0
Enrique Alvarez <Enrique@Enriques-MacBook-Pro.local>
2018-08-07 21:47:23 -0500

    Peticiones

---
+++ Peticiones/modulo.js
@@ -0,0 +1,18 @@
+// let numero = 100;
+
+// let funcion = () => {
+//     console.log("Soy una funcion");
+// }
+
+// module.exports = funcion;
+
+let miObjeto = {
+    sumar : function (num1,num2) {
+        return num1+num2;
+    },
+    restar : (num1,num2) => {
+        return num1-num2;
+    }
+};
+
+module.exports = miObjeto;
